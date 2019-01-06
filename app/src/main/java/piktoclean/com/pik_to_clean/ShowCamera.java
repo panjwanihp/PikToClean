b9b6b3a3b542b0e11c3d1fdfc83916265c3487d3
IMHARSH\HarshPanjwani <panjwani28.harsh@gmail.com>
2019-01-06 13:44:52 +0530

    sliderAdapetr completed

--- app/src/main/java/piktoclean/com/pik_to_clean/ShowCamera.java
+++ app/src/main/java/piktoclean/com/pik_to_clean/ShowCamera.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.content.res.Configuration;
 import android.hardware.Camera;
 import android.support.v4.content.res.ConfigurationHelper;
+import android.util.Log;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
@@ -28,28 +29,45 @@ public class ShowCamera extends SurfaceView implements SurfaceHolder.Callback{
     public void surfaceCreated(SurfaceHolder holder) {
         Camera.Parameters params = cam.getParameters();
         List<Camera.Size>  size = params.getSupportedPictureSizes();
-        Camera.Size m=null;
+        Camera.Size m=size.get(0);
+        Log.d("harshlk", "surfaceCreated: 1");
         for(Camera.Size sizes : size){
+            Log.d("harshlk", "surfaceCreated: 2");
             m=sizes;
-        }
-        if (this.getResources().getConfiguration().orientation != Configuration.ORIENTATION_LANDSCAPE) {
-            params.set("orientation", "potrait");
-            cam.setDisplayOrientation(90);
-        params.setRotation(90);
-        } else {
-            params.set("orientation", "Landscape");
-            cam.setDisplayOrientation(0);
-          params.setRotation(0);
-        }
 
+        }
+        Log.d("harshlk", "surfaceCreated: 3");
         params.setPictureSize(m.width,m.height);
+        Log.d("harshlk", "surfaceCreated: 4");
+     //   if (this.getResources().getConfiguration().orientation != Configuration.ORIENTATION_LANDSCAPE) {
+    //        Log.d("harshlk", "surfaceCreated: 5");
+          //  params.set("orientation", "potrait");
+           // Log.d("harshlk", "surfaceCreated: 6");
+            cam.setDisplayOrientation(90);
+           // Log.d("harshlk", "surfaceCreated: 7");
+            params.setRotation(90);
+  //      } else {
+//            Log.d("harshlk", "surfaceCreated: 5");
+//            params.set("orientation", "Landscape");
+//            Log.d("harshlk", "surfaceCreated: 6");
+//            cam.setDisplayOrientation(0);
+//            Log.d("harshlk", "surfaceCreated: 7");
+//            params.setRotation(0);
+      //  }
+        Log.d("harshlk", "surfaceCreated: 8");
+        params.setPictureSize(m.width,m.height);
+        Log.d("harshlk", "surfaceCreated: 9");
         cam.setParameters(params);
         try {
+            Log.d("harshlk", "surfaceCreated: 10");
             cam.setPreviewDisplay(holder);
+            Log.d("harshlk", "surfaceCreated: 11");
             cam.startPreview();
 
         }catch (IOException e){
-            e.printStackTrace();
+            Log.d("harshlk", "surfaceCreated: 11");
+            Log.d("harshlk", "surfaceCreated: "+e);
+
         }
     }
     @Override
--- app/src/main/java/piktoclean/com/pik_to_clean/fragment/CamFragment.java
+++ app/src/main/java/piktoclean/com/pik_to_clean/fragment/CamFragment.java
@@ -2,6 +2,7 @@ package piktoclean.com.pik_to_clean.fragment;
 
 
 import android.annotation.SuppressLint;
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
@@ -18,6 +19,23 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.FrameLayout;
 
+import android.Manifest;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
+import android.provider.Settings;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
 
 import com.google.protobuf.compiler.PluginProtos;
 
@@ -26,6 +44,7 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
+import piktoclean.com.pik_to_clean.MainActivity;
 import piktoclean.com.pik_to_clean.R;
 import piktoclean.com.pik_to_clean.ShowCamera;
 import piktoclean.com.pik_to_clean.classification;
@@ -35,10 +54,15 @@ import piktoclean.com.pik_to_clean.classification;
  */
 public class CamFragment extends Fragment {
     private android.hardware.Camera camera;
+    private static final int REQUEST_LOCATION = 1;
+    private static final int MY_CAMERA_REQUEST_CODE = 100;
     private FrameLayout fl;
     private ShowCamera showCamera;
     private Context c;
     private FloatingActionButton fab;
+    LocationManager locationManager;
+    String lattitude,longitude;
+    private View v2;
     public CamFragment() {
         // Required empty public constructor
     }
@@ -50,6 +74,7 @@ public class CamFragment extends Fragment {
         // Inflate the layout for this fragment
         c=inflater.getContext();
         return inflater.inflate(R.layout.fragment_cam, container, false);
+
     }
 
     @Override
@@ -59,14 +84,31 @@ public class CamFragment extends Fragment {
         fab=getView().findViewById(R.id.cap);
         camera= android.hardware.Camera.open();
         showCamera = new ShowCamera(c,camera);
+        ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, REQUEST_LOCATION);
+        ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.CAMERA},MY_CAMERA_REQUEST_CODE);
+        ActivityCompat.requestPermissions(getActivity(), new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_LOCATION);
         fl.addView(showCamera);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                captureImage(v);
+                v2=v;
+                locationManager = (LocationManager) getActivity().getSystemService(Context.LOCATION_SERVICE);
+                if (!locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+                    buildAlertMessageNoGps();
+
+                } else if (locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+                    String check = getLocation();
+                    if (check == "") {
+                    } else {
+                        captureImage(v);
+                    }
+                }
+
             }
         });
     }
+
+
     android.hardware.Camera.PictureCallback mPictureCallback = new android.hardware.Camera.PictureCallback() {
 
 
@@ -80,6 +122,79 @@ public class CamFragment extends Fragment {
         }
     };
 
+
+    private String getLocation() {
+        if (ActivityCompat.checkSelfPermission(c, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission
+                (c, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+            ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_LOCATION);
+            return "";
+        } else {
+            Location location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+
+            Location location1 = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+
+            Location location2 = locationManager.getLastKnownLocation(LocationManager.PASSIVE_PROVIDER);
+
+            if (location != null) {
+                double latti = location.getLatitude();
+                double longi = location.getLongitude();
+                lattitude = String.valueOf(latti);
+                longitude = String.valueOf(longi);
+
+                Toast.makeText(c, "Lattitude = " + lattitude
+                        + "\n" + "Longitude = " + longitude, Toast.LENGTH_SHORT).show();
+                return "Lattitude = " + lattitude
+                        + "\n" + "Longitude = " + longitude;
+            } else if (location1 != null) {
+                double latti = location1.getLatitude();
+                double longi = location1.getLongitude();
+                lattitude = String.valueOf(latti);
+                longitude = String.valueOf(longi);
+
+                Toast.makeText(c, "Lattitude = " + lattitude
+                        + "\n" + "Longitude = " + longitude, Toast.LENGTH_SHORT).show();
+                return "Lattitude = " + lattitude
+                        + "\n" + "Longitude = " + longitude;
+
+            } else if (location2 != null) {
+                double latti = location2.getLatitude();
+                double longi = location2.getLongitude();
+                lattitude = String.valueOf(latti);
+                longitude = String.valueOf(longi);
+
+                Toast.makeText(c, "Lattitude = " + lattitude
+                        + "\n" + "Longitude = " + longitude + "", Toast.LENGTH_SHORT).show();
+                return "Lattitude = " + lattitude
+                        + "\n" + "Longitude = " + longitude;
+            } else {
+
+                Toast.makeText(c, "Unble to Trace your location", Toast.LENGTH_SHORT).show();
+                return "";
+            }
+        }
+    }
+
+    protected void buildAlertMessageNoGps() {
+
+        final AlertDialog.Builder builder = new AlertDialog.Builder(c);
+        builder.setMessage("Please Turn ON your GPS Connection")
+                .setCancelable(false)
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    public void onClick(final DialogInterface dialog, final int id) {
+                        startActivity(new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS));
+
+                    }
+                })
+                .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    public void onClick(final DialogInterface dialog, final int id) {
+                        dialog.cancel();
+                    }
+                });
+        final AlertDialog alert = builder.create();
+        alert.show();
+    }
 
 
     public void captureImage(View v){
